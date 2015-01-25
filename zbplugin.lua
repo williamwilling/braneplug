9c8f46af94decb7391f11e11c850554f090e236b
William Willing <william@williamwilling.com>
2015-01-25 11:15:41 +0100

    Updated the plugin installation script.

--- zbplugin.lua
+++ zbplugin.lua
@@ -1,10 +1,12 @@
 return {
   description = "A plugin manager for ZeroBrane Studio.",
   author = "William Willing",
-  version = 1,
-  
+  version = 2,
+
   install = function()
     local remotePath = "http://zerobranestore.blob.core.windows.net/braneplug/"
     download(remotePath .. "braneplug.lua", idePath .. "packages/braneplug.lua")
+    download(remotePath .. "done.png", idePath .. "packages/done.png")
+    download(remotePath .. "installing.png", idePath .. "packages/installing.png")
   end
 }
